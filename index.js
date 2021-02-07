fb1f0eacb728141eeaed274891030e5ba77cd236
Evan <evanfujita@gmail.com>
2021-02-06 21:16:04 -0600

    commit tree 8714ba2ea1079c3d3bbff95698afc3cf2346f3ad

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching (array, name) {
+    let result = array.filter( driver => {
+       return driver.toUpperCase() == name.toUpperCase()
+    })
+    return result;
+}
+
+let fuzzyMatch = (array, string) => {
+    let result = array.filter(driver => {
+        return driver.includes(string)
+        
+    })
+    return result
+}
+
+function matchName (array, name) {
+    return array.filter (driver => driver.name === name)
+}
+
